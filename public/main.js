f088ddc2d03d887187e4f6e2ab25356bde8598aa
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-04-20 22:24:43 -0300

    inital commit

---
+++ public/main.js
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from "electron";
+import { join } from "path";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadURL("http://localhost:3000");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});

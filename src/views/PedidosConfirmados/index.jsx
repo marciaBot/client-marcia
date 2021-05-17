80aa45c0bb8f44b13d0dda81a082c4353daafa4d
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-05-17 11:37:49 -0300

    criação da tela de pedidos confirmados

--- src/views/PedidosConfirmados/index.jsx
+++ src/views/PedidosConfirmados/index.jsx
@@ -1,8 +1,31 @@
 import React from "react";
-import "./styles";
+import {
+  TopContent,
+  Container,
+  Main,
+  PageTitle,
+  BottomContent,
+} from "./styles";
+import Navbar from "../../components/Navbar";
+import SearchBar from "../../components/SearchBar";
+import DashboardButton from "../../components/DashboardButton";
 
 function PedidosConfirmados() {
-  return <h1>Pedidos Confirmados</h1>;
+  return (
+    <Container>
+      <Navbar />
+      <Main>
+        <TopContent>
+          <PageTitle>Pedidos Confirmados</PageTitle>
+          <SearchBar />
+        </TopContent>
+        <BottomContent>
+          <DashboardButton />
+          <DashboardButton />
+        </BottomContent>
+      </Main>
+    </Container>
+  );
 }
 
 export default PedidosConfirmados;

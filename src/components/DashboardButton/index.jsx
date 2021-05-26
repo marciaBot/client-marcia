8e0608e4baf5aeb50e5844bfb360ce0d808b8d2f
Pedro Lima <pedro.lima.wk@gmail.com>
2021-05-26 17:12:50 -0300

    feat: Adicionando chamada da API de forma correta

--- src/components/DashboardButton/index.jsx
+++ src/components/DashboardButton/index.jsx
@@ -1,20 +1,45 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import ModalConfirmados from "../Modal/PedidosConfirmados";
 import ModalConfirmar from "../Modal/PedidosConfirmar";
 import { Container, ButtonContent } from "./styles";
+import api from "../../services/api";
 
 function DashboardButton(props) {
   const [mostrarModal, setMostrarModal] = useState(false);
+  const [itens, setItens] = useState([]);
+  const [quantidade, setQuantidade] = useState(0);
+  const [telefone, setTelefone] = useState(0);
+  const [obs, setObs] = useState("");
 
-  const abrirModal = () => {
+  const abrirModal = (id, clienteId) => {
+    api.get(`venda/listar-itens-venda/${id}`).then((response) => {
+      const itensData = response.data.itemList;
+      const data = itensData.reduce((total, item) => {
+        return total += item.quantidade;
+      }, 0);
+      setQuantidade(data);
+      setItens(itensData);
+    });
+
+    api.get(`cliente/${clienteId}`).then((response) => {
+      setTelefone(response.data.numero);
+    });
+
+    api.get(`venda/${id}`).then((response) => {
+      setObs(response.data.obs);
+    });
     setMostrarModal((prev) => !prev);
   };
 
   return (
-    <Container onClick={abrirModal}>
-      <ButtonContent>
+    <Container>
+      <ButtonContent onClick={() => abrirModal(props.id, props.clienteId)}>
         <span>{props.nome}</span>
         <div>
           <p>
@@ -27,11 +52,24 @@ function DashboardButton(props) {
         <ModalConfirmados
           mostrarModal={mostrarModal}
           setMostrarModal={setMostrarModal}
+          itensData={itens}
+          nome={props.nome}
+          valor={props.valor}
+          id={props.id}
+          quantidade={quantidade}
+          telefone={telefone}
+          obs={obs}
         />
       ) : (
         <ModalConfirmar
           mostrarModal={mostrarModal}
           setMostrarModal={setMostrarModal}
+          itensData={itens}
+          nome={props.nome}
+          valor={props.valor}
+          id={props.id}
+          quantidade={quantidade}
+          telefone={telefone}
         />
       )}
     </Container>
